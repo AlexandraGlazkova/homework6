734b967965f782b4217db10d955f8f3727c83742
GlakovaA <22109@bk.ru>
2022-07-21 00:55:32 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -18,11 +19,31 @@ public class Main {
 //    Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
 //    в формате: «Сумма трат за месяц составила … рублей».
         System.out.println("Задание 1");
-        int sum =0;
+        int sum =arr[0];
         for (int j : arr) {
             sum += j;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+//        Задание 2
+//        Следующая задача — найти минимальную и максимальную трату за день.
+//        Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+//        в формате: «Минимальная сумма трат за день составила … рублей. Максимальная сумма трат
+//        за день составила … рублей»
+        System.out.println("Задание 2");
+        int max = arr[0];
+        int min = arr[arr.length-1];
+        for (int i=1; i<arr.length-1; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + max +  " рублей");
 
 
     }
